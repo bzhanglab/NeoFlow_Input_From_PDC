90398cae60ebaa82e8192e823a4cd601e4bbe149
KaiLiCn <likaicnsh@gmail.com>
2021-04-01 17:49:11 -0500

    Add files via upload

--- generate_mapping_table.R
+++ generate_mapping_table.R
@@ -4,8 +4,16 @@ args <- commandArgs(T)
 pdc_table_file <- args[1]
 mapping_table_file <- args[2]
 fusion_link <- args[3]
-output_file <- args[4]
-add_or <- args[5]
+data_ava <- read.delim(args[4], check.names = FALSE)
+output_file <- args[5]
+add_or <- args[6]
+
+data_ava_ga <- gather(data_ava, sample, value, 2: ncol(data_ava))
+data_ava_sp <- spread(data_ava_ga, idx, value) %>%
+        filter(WES == "Y", Proteomics == "Y")
+
+data_ava_sp_no_fu <- data_ava_sp %>%
+        filter(Gene_fusion == "N")
 
 pdc_table <- read.delim(pdc_table_file)
 colnames(pdc_table) <- c("experiment_id", "mzml_files", "mzml_links")
@@ -18,6 +26,8 @@ if (add_or == "T"){
 } else {
 	output_data$fusion <- paste(fusion_link, output_data$sample, "/fusions.tsv", sep="")
 }
+output_data <- output_data %>% filter(sample %in% data_ava_sp$sample)
+output_data$fusion <- ifelse(output_data$sample %in% data_ava_sp_no_fu$sample, "NA", output_data$fusion)
 output_data$sample <- paste(output_data$experiment, output_data$sample, sep="_")
 
 write.table(output_data, output_file, quote = F, row.names = F, sep="\t")
--- run_generate_mapping_table.sh
+++ run_generate_mapping_table.sh
@@ -14,18 +14,20 @@ set -e -x
 ###3. Case list used in PGET project
 ###Output:
 ###1. Pre-mapping table
-Rscript prepare_mapping.R /data/kail/2020_11_neoflow2/neoflow2/PGET_data/experiment_design_pdc/PDC_study_experimental_HNSCC.csv /data/kail/2020_11_neoflow2/neoflow2/PGET_data/wxs_gdc_manifest/CPTAC3-WXS_bam-gdc_manifest.2020-10-27.metadata.files_HNSCC.txt /data/PGET_data_freeze_v1.0/data_freeze_v1.0/HNSCC/CPTAC3_HNSCC_Tumor_CaseList.txt ./HNSCC_pre_mapping_table.txt
+Rscript prepare_mapping.R /data/kail/2020_11_neoflow2/neoflow2/PGET_data/experiment_design_pdc/PDC_study_experimental_BRCA.csv /data/kail/2020_11_neoflow2/neoflow2/PGET_data/wxs_gdc_manifest/CPTAC2-WXS_bam-gdc_manifest.2020-12-18.metadata.files_breast.txt /data/PGET_data_freeze_v1.0/data_freeze_v1.0/BRCA/CPTAC2_BRCA_Tumor_CaseList.txt ./BRCA_pre_mapping_table.txt
 
 ###Input:
 ###1. Mzml link file downloaded from PDC website
 ###Output
 ###1. Processed MZml link files
-Rscript process_gdc_table.R ./PDC_file_manifest_03022021_153834.csv ./PDC_file_manifest_processed_table.txt
+Rscript process_gdc_table.R ./PDC_file_manifest_03262021_130720.csv ./PDC_file_manifest_processed_table.txt
 
 ###Input
 ###1. Processed MZml link files from second step
 ###2. Pre-mapping table from first step
 ###3. Fusion results link
+###4. Data avalible table
+###5. Add "_" or not
 ###Output
 ###1. Final mapping table
-Rscript generate_mapping_table.R ./PDC_file_manifest_processed_table.txt ./HNSCC_pre_mapping_table.txt fusion/ ./HNSCC_mapping_table.txt T
+Rscript generate_mapping_table.R ./PDC_file_manifest_processed_table.txt ./BRCA_pre_mapping_table.txt fusion/ /data/PGET_data_freeze_v1.0/data_available_table_v1.0/BRCA_confirmatory-data-availability.txt ./BRCA_mapping_table.txt F
